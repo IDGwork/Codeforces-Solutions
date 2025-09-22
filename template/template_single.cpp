0b003d9194883a4b972c453527f3cfbc811ff02c
CP Training <cptraining@Ignass-MacBook-Pro.local>
2025-09-22 14:15:59 +0100

    Initial commit: setup Codeforces-Solutions with CMake + templates

---
+++ template/template_single.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+using ll = long long;
+
+int main() {
+    fastio;
+    // solve single test
+    return 0;
+}
---
+++ template/template_tc.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+using ll = long long;
+
+int main() {
+    fastio;
+    int T;
+    if (!(cin >> T)) return 0;
+    while (T--) {
+        // solve one test case
+    }
+    return 0;
+}
